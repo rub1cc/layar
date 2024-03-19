a3053c6a3777038a2418e25707cedd3190b0d676
Faraz Aulia <faraz.aulia@aleph-labs.com>
2024-03-19 14:00:16 +0700

    disable devtools on production

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,6 +14,7 @@ function createWindow(): void {
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
+      devTools: is.dev,
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
       webviewTag: true,
