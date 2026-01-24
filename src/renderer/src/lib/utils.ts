c5d94b400e7183204d256aba0661bfbe3823953e
Faraz Aulia <hey@rub1.cc>
2026-01-24 12:00:00 +0700

    allow localhost to open directly

--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -17,6 +17,7 @@ export function isValidURL(str: string): boolean {
   const regex = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      'localhost|' + // localhost
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
